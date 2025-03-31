f006a73c3c038ad6f7ed635d50629205690375ea
SuperexMack <unilexerox@gmail.com>
2025-03-31 16:50:55 +0530

    Add Backend logic

--- Backend/Controller/WebRTCBackend.js
+++ Backend/Controller/WebRTCBackend.js
@@ -1,13 +1,31 @@
 const {WebSocketServer} = require("ws")
 
 
+let rooms = {};
 
     const mainFunction = (server)=>{
         const wss = new WebSocketServer({server})
         wss.on("connection" , function connection(ws){
             ws.on("message" , function getData(data){
                 console.log("Everything is fine")
-                console.log(JSON.parse(data))
+                let alldata = JSON.parse(data)
+                if(alldata.type === "Create"){
+                   rooms[alldata.room] = []
+                   rooms[alldata.room].push(ws);
+                   ws.room = alldata.room
+                }
+                else if(alldata.type === "Join"){
+                   rooms[alldata.room] = rooms[alldata.room] || []
+                   rooms[alldata.room].push(ws);
+                   ws.room = alldata.room
+                }
+                else if(alldata.type === "Chat"){
+                    if(rooms[alldata.room]){
+                        rooms[alldata.room].forEach((msg)=>{
+                            msg.send(JSON.stringify({type : "Chat" , newMsg : alldata.mymsg , toRoom : alldata.room}))
+                        })
+                    }
+                }
             })
         })
     }
