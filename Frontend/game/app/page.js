55d5b168d4673e40bddde76ef19b896ba5dd1a97
SuperexMack <unilexerox@gmail.com>
2025-03-31 12:00:07 +0530

    Add Room page

--- Frontend/game/app/page.js
+++ Frontend/game/app/page.js
@@ -2,8 +2,18 @@
 export default function Home() {
   return (
     <>
-      <div className="w-full min-h-screen bg-slate-300">
-        
+      <div className="w-full min-h-screen flex justify-center items-center">
+
+        <div className="w-[400px] h-auto p-6 bg-slate-100 shadow-2xl shadow-slate-400 rounded-lg flex flex-col items-center space-y-8">
+          <h1 className="mt-4 font-bold text-[25px]">Room Details</h1>
+          <input className="w-[80%] border-2 rounded-lg border-yellow-400 p-2" placeholder="John Doe"></input>
+          <input className="w-[80%] border-2 rounded-lg border-yellow-400 p-2" placeholder="Room Name"></input>
+          
+          <button className="w-[80%]  bg-blue-600 p-2 rounded-lg font-bold text-white">Create Room</button>
+          <button className="w-[80%]  bg-blue-600 p-2 rounded-lg font-bold text-white">Join Room</button>
+
+        </div>
+
       </div>
     </>
   );
